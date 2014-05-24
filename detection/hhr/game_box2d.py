3f436c76e665c9ce239a9aed6ab918df2854b9f5
Omwah <omwah@oyeh.org>
2014-05-23 22:07:51 -0700

    Move box2d game logic into seperate file

---
+++ detection/hhr/game_box2d.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+
+from Box2D import *
+
+from .sim_box2d import AirHockeyTable, PPM
+
+TARGET_FPS=60
+TIME_STEP=1.0/TARGET_FPS
+
+colors = {
+    b2_staticBody  : (255,255,255,255),
+    b2_dynamicBody : (127,127,127,255),
+    b2_kinematicBody: (127,127,230,255),
+    'mouse_point'     : b2Color(0,1,0),
+    'joint_line'      : b2Color(0.8,0.8,0.8),
+}
+
+class fwQueryCallback(b2QueryCallback):
+    def __init__(self, p): 
+        super(fwQueryCallback, self).__init__()
+        self.point = p
+        self.fixture = None
+
+    def ReportFixture(self, fixture):
+        body = fixture.body
+        if body.type == b2_dynamicBody:
+            inside=fixture.TestPoint(self.point)
+            if inside:
+                self.fixture=fixture
+                # We found the object, so stop the query
+                return False
+        # Continue the query
+        return True
+
+class AirHockeyGame(AirHockeyTable):
+
+    def __init__(self, screen_width, screen_height):
+        super(AirHockeyGame, self).__init__(screen_width, screen_height)
+
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+        # pygame setup
+        self.screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+        self.clock = pygame.time.Clock()
+
+        self.mouseJoint = None
+        self.groundbody = self.world.CreateBody()
+
+        self.add_player()
+
+        self.draw = {
+            b2PolygonShape: self.draw_polygon,
+            b2CircleShape: self.draw_circle,
+            b2EdgeShape: self.draw_edge,
+            b2LoopShape: self.draw_loop,
+        }
+
+    def add_player(self):
+        self.players.append( self.add_circle(b2Vec2(self.width - self.width / 8, self.height / 2) + self.offset, 11.0 / PPM, 3) )
+
+    def remove_player(self, player):
+        if not player in self.players:
+            raise Exception("object not a player")
+
+        self.world.DestroyBody(player)
+        self.players.remove(player)
+
+    def add_mouse_joint(self, p):
+        "Indicates that there was a left click at point p (world coordinates)"
+
+        if self.mouseJoint != None:
+            return
+
+        # Create a mouse joint on the selected body (assuming it's dynamic)
+        # Make a small box.
+        aabb = b2AABB(lowerBound=p-(0.001, 0.001), upperBound=p+(0.001, 0.001))
+
+        # Query the world for overlapping shapes.
+        query = fwQueryCallback(p)
+        self.world.QueryAABB(query, aabb)
+        
+        if query.fixture:
+            body = query.fixture.body
+            # A body was selected, create the mouse joint
+            self.mouseJoint = self.world.CreateMouseJoint(
+                    bodyA=self.groundbody,
+                    bodyB=body, 
+                    target=p,
+                    maxForce=1000.0*body.mass)
+            body.awake = True
+
+    def remove_mouse_joint(self, p):
+        "Left mouse button up."     
+
+        if self.mouseJoint:
+            self.world.DestroyJoint(self.mouseJoint)
+            self.mouseJoint = None
+
+    def update_mouse_joint(self, p):
+        "Mouse moved to point p, in world coordinates."
+        self.mouseWorld = p
+        if self.mouseJoint:
+            self.mouseJoint.target = p
+
+    def to_world(self, x, y):
+        return b2Vec2(x / PPM, (self.screen_height - y) / PPM)
+
+    def to_screen(self, point):
+        return ( int(point.x) * PPM, int(self.screen_height - point.y * PPM) )
+
+    def fix_vertices(self, vertices):
+        return [self.to_screen(v) for v in vertices]
+
+    def draw_polygon(self, polygon,  body, fixture):
+        transform=body.transform
+        vertices=self.fix_vertices([transform*v for v in polygon.vertices])
+        pygame.draw.polygon(self.screen, [c/2.0 for c in colors[body.type]], vertices, 0)
+        pygame.draw.polygon(self.screen, colors[body.type], vertices, 1)
+
+    def draw_circle(self, circle, body, fixture):
+        position=self.fix_vertices([body.transform*circle.pos])[0]
+        pygame.draw.circle(self.screen, colors[body.type], position, int(circle.radius*PPM))
+
+    def draw_edge(self, edge, body, fixture):
+        vertices=self.fix_vertices([body.transform*edge.vertex1, body.transform*edge.vertex2])
+        pygame.draw.line(self.screen, colors[body.type], vertices[0], vertices[1])
+
+    def draw_loop(self, loop, body, fixture):
+        transform=body.transform
+        vertices=self.fix_vertices([transform*v for v in loop.vertices])
+        v1=vertices[-1]
+        for v2 in vertices:
+            pygame.draw.line(self.screen, colors[body.type], v1, v2)
+            v1=v2
+
+    def render(self):
+        # Check the event queue
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                # The user closed the window or pressed escape
+                return False
+            elif event.type == MOUSEBUTTONDOWN:
+                p = self.to_world(*event.pos)
+                if event.button == 1: # left
+                    mods = pygame.key.get_mods()
+                    self.add_mouse_joint(p)
+            elif event.type == MOUSEBUTTONUP:
+                p = self.to_world(*event.pos)
+                self.remove_mouse_joint(p)
+            elif event.type == MOUSEMOTION:
+                p = self.to_world(*event.pos)
+                self.update_mouse_joint(p)
+
+        # Remove old and add new puck if it goes out of the table
+        for puck in self.pucks:
+            if puck.position[0] < self.offset[0] or puck.position[0] > (self.width + self.offset[0]):
+                self.remove_puck(puck)
+                self.add_puck()
+
+        # Remove old and add new player if it goes out of the table
+        for player in self.players:
+            if player.position[0] < self.offset[0] or player.position[0] > (self.width + self.offset[0]):
+                self.remove_player(player)
+                self.add_player()
+
+        self.screen.fill((0,0,0,0))
+        # Draw the world
+        for body in self.world.bodies:
+            # The body gives us the position and angle of its shapes
+            for fixture in body.fixtures:
+                self.draw[fixture.shape.__class__](fixture.shape, body, fixture)
+
+        # If there's a mouse joint, draw the connection between the object and the current pointer position.
+        if self.mouseJoint:
+            p1 = self.to_screen(self.mouseJoint.anchorB)
+            p2 = self.to_screen(self.mouseJoint.target)
+
+            pygame.draw.circle(self.screen, colors['mouse_point'].bytes, p1, 2, 1)
+            pygame.draw.circle(self.screen, colors['mouse_point'].bytes, p2, 2, 1)
+            pygame.draw.aaline(self.screen, colors['joint_line'].bytes, p1, p2)
+
+        # Make Box2D simulate the physics of our world for one step.
+        # Instruct the world to perform a single step of simulation. It is
+        # generally best to keep the time step and iterations fixed.
+        # See the manual (Section "Simulating the World") for further discussion
+        # on these parameters and their implications.
+        self.world.Step(TIME_STEP, 6, 2)
+
+        # Flip the screen and try to keep at the target FPS
+        pygame.display.flip()
+        self.clock.tick(TARGET_FPS)
+
+        return True
+
+if __name__=="__main__":
+    ah_game = AirHockeyGame(320, 240)
+
+    running = True
+    while running:
+        running = ah_game.render()
